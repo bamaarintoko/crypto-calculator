2cf12a329f9414648dd22e2078331ab8fd5b6afa
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-06-16 01:10:06 +0700

    add status bar

--- src/Screen/Home/screen-home.js
+++ src/Screen/Home/screen-home.js
@@ -4,7 +4,7 @@ import {
     Platform,
     StyleSheet,
     Text,
-    View, FlatList
+    View, FlatList, StatusBar
 } from 'react-native';
 import { Container, Content, Item, Input, Button } from "native-base"
 function mapStateToProps(state) {
@@ -23,45 +23,48 @@ _onMore = () => {
 
 }
 
-_btnClick = ()=>{
+_btnClick = () => {
 
 }
 class ScreenHome extends Component {
     render() {
         let firstQuery = ""
         return (
-            <Container>
-
+            <Container style={{ backgroundColor: '#1565C0' }}>
+                <StatusBar
+                    backgroundColor="#1565C0"
+                    barStyle="light-content"
+                />
                 <Content>
-                    <View style={{backgroundColor:'blue',height:60}}>
+                    <View style={{ backgroundColor: '#1565C0', height: 60 }}>
 
                     </View>
-                    <View style={{height:200}}>
+                    <View style={{ height: 200 }}>
 
                     </View>
-                    <View style={{ flex: 1, flexDirection: 'row', backgroundColor:'#FFF', marginLeft:30, marginRight:30,marginTop:10, borderRadius:5 }}>
+                    <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#FFF', marginLeft: 30, marginRight: 30, marginTop: 10, borderRadius: 5 }}>
                         <View style={{ width: 30 + '%' }}>
 
-                            <Button full info style={{borderTopLeftRadius:5,borderBottomLeftRadius:5}}>
+                            <Button full info style={{ borderTopLeftRadius: 5, borderBottomLeftRadius: 5 }}>
                                 <Text>IDR</Text>
                             </Button>
                         </View>
                         <View style={{ width: 70 + '%' }}>
-                            <Item regular style={{ borderColor: '#FFF', backgroundColor: '#FFF', height:45 }}>
-                                <Input style={{fontSize:14}} placeholder='Regular Textbox' />
+                            <Item regular style={{ borderColor: '#FFF', backgroundColor: '#FFF', height: 45 }}>
+                                <Input style={{ fontSize: 14 }} placeholder='Regular Textbox' />
                             </Item>
                         </View>
                     </View>
-                    <View style={{ flex: 1, flexDirection: 'row', backgroundColor:'#FFF', margin:30 }}>
+                    <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#FFF', margin: 30, borderRadius: 5 }}>
                         <View style={{ width: 30 + '%' }}>
 
-                            <Button full info style={{borderTopLeftRadius:5,borderBottomLeftRadius:5}}>
+                            <Button full info style={{ borderTopLeftRadius: 5, borderBottomLeftRadius: 5 }}>
                                 <Text>BTC</Text>
                             </Button>
                         </View>
                         <View style={{ width: 70 + '%' }}>
-                            <Item regular style={{ borderColor: '#FFF', backgroundColor: '#FFF', height:45 }}>
-                                <Input style={{fontSize:14}} placeholder='Regular Textbox' />
+                            <Item regular style={{ borderColor: '#FFF', backgroundColor: '#FFF', height: 45 }}>
+                                <Input style={{ fontSize: 14 }} placeholder='Regular Textbox' />
                             </Item>
                         </View>
                     </View>
