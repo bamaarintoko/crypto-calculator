276c5b083962b9c364b911851b0807565d22f830
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-06-16 00:56:52 +0700

    add group input

--- src/Screen/Home/screen-home.js
+++ src/Screen/Home/screen-home.js
@@ -6,7 +6,7 @@ import {
     Text,
     View, FlatList
 } from 'react-native';
-import {Container,Content} from "native-base"
+import { Container, Content, Item, Input, Button } from "native-base"
 function mapStateToProps(state) {
     return {
 
@@ -27,9 +27,38 @@ class ScreenHome extends Component {
         let firstQuery = ""
         return (
             <Container>
-                
+
                 <Content>
-                    <Text>Haloooo</Text>
+                    <View style={{backgroundColor:'blue',height:60}}>
+
+                    </View>
+                    <View style={{ flex: 1, flexDirection: 'row', backgroundColor:'#FFF', marginLeft:30, marginRight:30,marginTop:10, borderRadius:5 }}>
+                        <View style={{ width: 30 + '%' }}>
+
+                            <Button full info style={{borderTopLeftRadius:5,borderBottomLeftRadius:5}}>
+                                <Text>IDR</Text>
+                            </Button>
+                        </View>
+                        <View style={{ width: 70 + '%' }}>
+                            <Item regular style={{ borderColor: '#FFF', backgroundColor: '#FFF', height:45 }}>
+                                <Input style={{fontSize:14}} placeholder='Regular Textbox' />
+                            </Item>
+                        </View>
+                    </View>
+                    <View style={{ flex: 1, flexDirection: 'row', backgroundColor:'#FFF', margin:30 }}>
+                        <View style={{ width: 30 + '%' }}>
+
+                            <Button full info style={{borderTopLeftRadius:5,borderBottomLeftRadius:5}}>
+                                <Text>BTC</Text>
+                            </Button>
+                        </View>
+                        <View style={{ width: 70 + '%' }}>
+                            <Item regular style={{ borderColor: '#FFF', backgroundColor: '#FFF', height:45 }}>
+                                <Input style={{fontSize:14}} placeholder='Regular Textbox' />
+                            </Item>
+                        </View>
+                    </View>
+
                 </Content>
             </Container>
         );
